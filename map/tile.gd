f9c96ffbe2eb4e485d4d31118a1b19704be44d80
DESKTOP-E11V55L\favl9 <stupin.siny@yandex.ru>
2024-04-06 11:54:16 +0300

    pls

--- map/tile.gd
+++ map/tile.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var buildScene: PackedScene = load("res://Components/Build.tscn")
 
 var lightPower: int = 0
 var building: Build
@@ -16,3 +17,12 @@ func set_pos_label(pos: Vector3):
 func _on_input_event(viewport, event: InputEvent, shape_idx):
 	if event.is_action_pressed("lmb"):
 		pass
+
+
+func set_build(type: int):
+	if type < 0 or type > 4:
+		return
+	var newBuild = buildScene.instantiate()
+	building = newBuild
+	newBuild.position = Vector2.ZERO
+	newBuild.set_build(type)
